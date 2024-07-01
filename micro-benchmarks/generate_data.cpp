a6ae1d024dace1e25527d0dd9c2381e3ecce0545
abdelrahman99 <ge49kos@mytum.de>
2024-07-02 01:21:59 +0200

    added data generator

--- micro-benchmarks/generate_data.cpp
+++ micro-benchmarks/generate_data.cpp
@@ -1,9 +1,36 @@
+#include <typeinfo>
+#include "utils/stopwatch.h"
+#include <boost/core/demangle.hpp>
 #include "storage/page/page.h"
-#include <iostream>
-#include <tuple>
 #include "utils/utils.h"
+#include "gflags/gflags.h"
 
-int main(){
-    Page<int64_t, int64_t, int32_t, char[4]> p{};
-    print("Size of page", sizeof(p));
+DEFINE_string(path, "", "path to output destination");
+DEFINE_int64(num_tuples, 100, "number of tuples to generate");
+
+template <typename... Attributes>
+void generate_data(){
+    Page<Attributes...> p{};
+    println("size of page:", sizeof(p), "( max tuples:", p.max_num_tuples_per_page, ")");
+    println("columns:", boost::core::demangle(typeid(Attributes).name())...);
+    {
+        Stopwatch _{};
+        // TODO count tuples properly
+        auto i = 0u;
+        for(; i < FLAGS_num_tuples; i+=p.max_num_tuples_per_page){
+            p.fill_random();
+            p.print_contents();
+        }
+
+    }
+    println("output path:", FLAGS_path);
+}
+
+int main(int argc, char* argv[]){
+    if (argc == 1) {
+        println("example usage: ./generate_data --path=path/to/output --num_tuples=10000");
+        exit(1);
+    }
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    generate_data<int64_t, int64_t, int32_t, unsigned char>();
 }
