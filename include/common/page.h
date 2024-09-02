d44d6fc05a06a3b677340e304762da91fa024fa9
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-09-02 13:39:41 +0200

    updated page struct

--- include/common/page.h
+++ include/common/page.h
@@ -29,20 +29,20 @@ void print_column(const std::array<T, length>& column, std::integral auto num_tu
 }
 
 // PAX-style page
-template <uint32_t page_size, typename... Attributes>
+template <uint64_t page_size, typename... Attributes>
 struct alignas(page_size) Page {
     static constexpr auto max_num_tuples_per_page = calc_max_num_tuples_per_page<Attributes...>(page_size);
-    alignas(8) uint32_t num_tuples{0};
+    uint64_t num_tuples{0};
     std::tuple<std::array<Attributes, max_num_tuples_per_page>...> columns;
 
-    template <uint32_t other_page_size, typename... OtherAttributes>
+    template <uint64_t other_page_size, typename... OtherAttributes>
     requires(sizeof...(Attributes) == 1 and sizeof...(OtherAttributes) == 1)
     void emplace_back(std::size_t row_idx, const Page<other_page_size, OtherAttributes...>& page) {
         std::get<0>(columns)[num_tuples] = std::get<0>(page.columns)[row_idx];
         num_tuples++;
     }
 
-    template <uint32_t other_page_size, typename... OtherAttributes,
+    template <uint64_t other_page_size, typename... OtherAttributes,
               typename Indices = std::index_sequence_for<OtherAttributes...>>
     // only allow column-store to row-store (transpose)
     requires(sizeof...(Attributes) == 1 and
@@ -52,7 +52,7 @@ struct alignas(page_size) Page {
         num_tuples++;
     }
 
-    template <uint32_t other_page_size, typename... OtherAttributes, std::size_t... indexes>
+    template <uint64_t other_page_size, typename... OtherAttributes, std::size_t... indexes>
     void emplace_back_transposed_helper(std::size_t row_idx, const Page<other_page_size, OtherAttributes...>& page,
                                         std::index_sequence<indexes...>) {
         // TODO
@@ -85,12 +85,12 @@ struct alignas(page_size) Page {
     void compress() {}
 };
 
-template <uint32_t size, typename... Attributes>
+template <uint64_t size, typename... Attributes>
 static auto as_bytes(Page<size, Attributes...>* page) {
     return reinterpret_cast<std::byte*>(page);
 }
 
-template <uint32_t page_size, typename... Attributes>
+template <uint64_t page_size, typename... Attributes>
 static void check_page(const Page<page_size, Attributes...> page, bool send) {
     for (auto i{0u}; i < page.max_num_tuples_per_page; ++i) {
         if (std::get<0>(std::get<0>(page.columns)[i]) != 0 and std::get<0>(std::get<0>(page.columns)[i]) != '0' and
@@ -98,7 +98,7 @@ static void check_page(const Page<page_size, Attributes...> page, bool send) {
             page.print_contents();
             println("weird character:", std::get<0>(std::get<0>(page.columns)[i]),
                     int32_t(std::get<0>(std::get<0>(page.columns)[i])),
-                    uint32_t(*reinterpret_cast<const uint32_t*>(&std::get<0>(page.columns)[i])), "idx:", i, "/",
+                    uint64_t(*reinterpret_cast<const uint64_t*>(&std::get<0>(page.columns)[i])), "idx:", i, "/",
                     page.max_num_tuples_per_page / 10, page.max_num_tuples_per_page);
             //            return;
             throw std::runtime_error(send ? "send unexpected page contents!!!"
