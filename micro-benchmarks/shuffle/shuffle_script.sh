75c3d57d5df1b569c82ffe6f37a17b11c5ad8cab
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-09-08 15:10:20 +0200

    updated shuffle_script.sh

--- micro-benchmarks/shuffle/shuffle_script.sh
+++ micro-benchmarks/shuffle/shuffle_script.sh
@@ -1,19 +1,22 @@
 TARGET_PATH='../../build-release/micro-benchmarks/shuffle/shuffle_homogeneous'
 LOCAL_NODE_ID="${NODE_ID:-0}"
 
-NODES=4
-FLAGS="--nolocal --random --npages=500000 --nodes=${NODES} --bufs_per_peer=2"
+MAX_NODES=4
+FLAGS="--nolocal --random --npages=500000 --bufs_per_peer=2"
 PRINT_HEADER='--print_header'
 
-for THREADS in 1 2 4 8 16 32 64;
+for NODES in $(seq $MAX_NODES -1 $(($LOCAL_NODE_ID+1)));
  do
-  for MORSEL_SZ in 10;
+  for THREADS in 1 2 4 8 16 32 64;
    do
-    $TARGET_PATH $PRINT_HEADER $FLAGS --threads=$THREADS --morselsz=$MORSEL_SZ > /dev/null
-    sleep 1s
-    if [[ "${LOCAL_NODE_ID}" == 0 ]]; then
+    for MORSEL_SZ in 10;
+     do
+      $TARGET_PATH $PRINT_HEADER $FLAGS --nodes=$NODES --threads=$THREADS --morselsz=$MORSEL_SZ > /dev/null
       sleep 1s
-    fi
-    PRINT_HEADER='--noprint_header'
-   done
+      if [[ "${LOCAL_NODE_ID}" == 0 ]]; then
+        sleep 1s
+      fi
+      PRINT_HEADER='--noprint_header'
+     done
+ done
 done
