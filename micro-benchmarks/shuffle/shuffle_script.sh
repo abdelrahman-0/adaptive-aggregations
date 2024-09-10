68f7e090335972f70d92c570a9cc638abf134d54
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-09-10 21:27:16 +0200

    added multiple-trys to shuffle script

--- micro-benchmarks/shuffle/shuffle_script.sh
+++ micro-benchmarks/shuffle/shuffle_script.sh
@@ -11,12 +11,15 @@ for NODES in $(seq $MAX_NODES -1 $(($LOCAL_NODE_ID+1)));
    do
     for BUFS_PER_PEER in $(seq 1 10);
      do
-      if [[ "${LOCAL_NODE_ID}" == 0 ]]; then
-        sleep 1s
-      fi
-      $TARGET_PATH $PRINT_HEADER $FLAGS --nodes=$NODES --threads=$THREADS --bufs_per_peer=$BUFS_PER_PEER > /dev/null
-      sleep 1s
-      PRINT_HEADER='--noprint_header'
+       for TRY in $(seq 1 5);
+        do
+          if [[ "${LOCAL_NODE_ID}" == 0 ]]; then
+            sleep 1s
+          fi
+          $TARGET_PATH $PRINT_HEADER $FLAGS --nodes=$NODES --threads=$THREADS --bufs_per_peer=$BUFS_PER_PEER > /dev/null
+          sleep 1s
+          PRINT_HEADER='--noprint_header'
+        done
      done
  done
 done
