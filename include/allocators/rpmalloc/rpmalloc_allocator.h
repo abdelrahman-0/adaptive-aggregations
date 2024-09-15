bec51e71273c002d69d4338feb3087179b718eb1
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-09-15 20:44:58 +0200

    added rpmalloc allocator

---
+++ include/allocators/rpmalloc/rpmalloc_allocator.h
@@ -0,0 +1,13 @@
+#include <cstdint>
+
+#include "allocators/rpmalloc/rpmalloc.h"
+
+template <typename T>
+struct RPMallocAllocator {
+    using value_type = T;
+    RPMallocAllocator() = default;
+    ~RPMallocAllocator() = default;
+
+    T* allocate(std::size_t n) { return static_cast<T*>(rpmalloc(n * sizeof(value_type))); }
+    void deallocate(T* p, std::size_t n) { rpfree(p); }
+};
