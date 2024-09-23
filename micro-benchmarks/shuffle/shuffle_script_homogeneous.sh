613c9cc9fbe4ac15d3ba2e167a622532bd77d769
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-09-23 12:26:49 +0200

    added heterogeneous shuffle script

---
+++ micro-benchmarks/shuffle/shuffle_script_homogeneous.sh
@@ -0,0 +1,21 @@
+TARGET_PATH='../../build-release/micro-benchmarks/shuffle/shuffle_homogeneous'
+LOCAL_NODE_ID="${NODE_ID:-0}"
+
+MAX_NODES=4
+FLAGS="--nolocal --random --npages=250000 --morselsz=10 --pin"
+PRINT_HEADER='--print_header'
+
+for NODES in $(seq $MAX_NODES -1 $(($LOCAL_NODE_ID + 1))); do
+  for THREADS in 1 4 6 12 24 30 32; do
+    for BUFS_PER_PEER in 4; do
+      for TRY in $(seq 1 5); do
+        if [[ "${LOCAL_NODE_ID}" == 0 ]]; then
+          sleep 1s
+        fi
+        $TARGET_PATH $PRINT_HEADER $FLAGS --nodes=$NODES --threads=$THREADS --bufs_per_peer=$BUFS_PER_PEER >/dev/null
+        sleep 1s
+        PRINT_HEADER='--noprint_header'
+      done
+    done
+  done
+done
