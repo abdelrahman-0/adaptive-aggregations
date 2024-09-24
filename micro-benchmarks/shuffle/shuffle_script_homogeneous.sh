1b70f7695cdde25780a35dd56dc1fa4c4793bbea
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-09-24 21:43:48 +0200

    updated shuffle scripts

--- micro-benchmarks/shuffle/shuffle_script_homogeneous.sh
+++ micro-benchmarks/shuffle/shuffle_script_homogeneous.sh
@@ -2,7 +2,7 @@ TARGET_PATH='../../build-release/micro-benchmarks/shuffle/shuffle_homogeneous'
 LOCAL_NODE_ID="${NODE_ID:-0}"
 
 MAX_NODES=4
-FLAGS="--nolocal --random --npages=250000 --morselsz=10 --pin"
+FLAGS="--nolocal --random --npages=500000 --morselsz=10 --pin"
 PRINT_HEADER='--print_header'
 
 for NODES in $(seq $MAX_NODES -1 $(($LOCAL_NODE_ID + 1))); do
