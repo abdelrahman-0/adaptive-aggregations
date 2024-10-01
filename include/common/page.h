82d9a23430a8094eba9db00b4c901b12e29d060e
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-10-01 09:05:55 +0200

    improved page emplace back functionality

--- include/common/page.h
+++ include/common/page.h
@@ -11,8 +11,8 @@
 #include "utils/utils.h"
 
 template <typename... Attributes>
-consteval std::size_t calc_max_num_tuples_per_page(std::size_t page_size) {
-    return ((page_size - sizeof(u64)) / (sizeof(Attributes) + ...));
+consteval std::size_t calc_max_tuples_per_page(std::size_t page_size) {
+    return ((page_size - alignof(std::max_align_t)) / (sizeof(Attributes) + ...));
 }
 
 template <typename T, std::size_t length>
@@ -30,33 +30,37 @@ void print_column(const std::array<T, length>& column, std::integral auto num_tu
 
 // PAX-style page
 template <u64 page_size, typename... Attributes>
-struct alignas(page_size) Page {
-    static constexpr auto max_num_tuples_per_page = calc_max_num_tuples_per_page<Attributes...>(page_size);
-    u64 num_tuples{0};
-    std::tuple<std::array<Attributes, max_num_tuples_per_page>...> columns;
+struct /* alignas(page_size) */ Page {
+    static constexpr auto max_tuples_per_page = calc_max_tuples_per_page<Attributes...>(page_size);
+    alignas(std::max_align_t) u64 num_tuples{0};
+    std::tuple<std::array<Attributes, max_tuples_per_page>...> columns;
 
-    template <u64 other_page_size, typename... OtherAttributes>
-    requires(sizeof...(Attributes) == 1 and sizeof...(OtherAttributes) == 1)
+    template <u64 idx, u64 other_idx, u64... other_idxs, u64 other_page_size, typename... OtherAttributes>
+    void _emplace_back(std::size_t row_idx, const Page<other_page_size, OtherAttributes...>& page) {
+        std::get<idx>(std::get<0>(columns)[num_tuples]) = std::get<other_idx>(std::get<0>(page.columns)[row_idx]);
+        if constexpr (idx != 0) {
+            _emplace_back<idx + 1, other_idxs...>(row_idx, page);
+        }
+    }
+
+    template <u64... other_idxs, u64 other_page_size, typename... OtherAttributes>
     void emplace_back(std::size_t row_idx, const Page<other_page_size, OtherAttributes...>& page) {
-        std::get<0>(columns)[num_tuples] = std::get<0>(page.columns)[row_idx];
+        _emplace_back<0, other_idxs...>(row_idx, page);
         num_tuples++;
     }
 
-    template <u64 other_page_size, typename... OtherAttributes,
-              typename Indices = std::index_sequence_for<OtherAttributes...>>
-    // only allow column-store to row-store (transpose)
-    requires(sizeof...(Attributes) == 1 and
-             custom_type_traits::is_tuple_v<std::tuple_element_t<0, std::tuple<Attributes...>>>)
-    void emplace_back_transposed(std::size_t row_idx, const Page<other_page_size, OtherAttributes...>& page) {
-        emplace_back_transposed_helper(row_idx, page, Indices{});
-        num_tuples++;
+    template <u64 idx, u64 other_idx, u64... other_idxs, u64 other_page_size, typename... OtherAttributes>
+    void _emplace_back_transposed(u64 row_idx, const Page<other_page_size, OtherAttributes...>& page) {
+        std::get<idx>(std::get<0>(columns)[num_tuples]) = std::get<other_idx>(page.columns)[row_idx];
+        if constexpr (idx != 0) {
+            _emplace_back_transposed<idx + 1, other_idxs...>(row_idx, page);
+        }
     }
 
-    template <u64 other_page_size, typename... OtherAttributes, std::size_t... indexes>
-    void emplace_back_transposed_helper(std::size_t row_idx, const Page<other_page_size, OtherAttributes...>& page,
-                                        std::index_sequence<indexes...>) {
-        //        std::get<0>(columns)[num_tuples] = std::tuple(std::get<indexes>(page.columns)[row_idx]...);
-        new (&(std::get<0>(columns)[num_tuples])) std::tuple(std::get<indexes>(page.columns)[row_idx]...);
+    template <u64... other_idxs, u64 other_page_size, typename... OtherAttributes>
+    void emplace_back_transposed(u64 row_idx, const Page<other_page_size, OtherAttributes...>& page) {
+        _emplace_back_transposed<0, other_idxs...>(row_idx, page);
+        num_tuples++;
     }
 
     void clear() { memset(this, 0, page_size); }
@@ -73,12 +77,12 @@ struct alignas(page_size) Page {
     }
 
     void print_info() const {
-        println<' '>("size of page:", page_size, "( max tuples:", max_num_tuples_per_page, ")");
+        println<' '>("size of page:", page_size, "( max tuples:", max_tuples_per_page, ")");
         print("schema: ");
         println<','>(boost::core::demangle(typeid(Attributes).name())...);
     }
 
-    [[nodiscard]] bool full() const { return num_tuples == max_num_tuples_per_page; }
+    [[nodiscard]] bool full() const { return num_tuples == max_tuples_per_page; }
 
     [[nodiscard]] bool empty() const { return num_tuples == 0; }
 
@@ -92,14 +96,14 @@ static auto as_bytes(Page<size, Attributes...>* page) {
 
 template <u64 page_size, typename... Attributes>
 static void check_page(const Page<page_size, Attributes...> page, bool send) {
-    for (auto i{0u}; i < page.max_num_tuples_per_page; ++i) {
+    for (auto i{0u}; i < page.max_tuples_per_page; ++i) {
         if (std::get<0>(std::get<0>(page.columns)[i]) != 0 and std::get<0>(std::get<0>(page.columns)[i]) != '0' and
             std::get<0>(std::get<0>(page.columns)[i]) != '1') {
             page.print_contents();
             println("weird character:", std::get<0>(std::get<0>(page.columns)[i]),
                     int32_t(std::get<0>(std::get<0>(page.columns)[i])),
                     u64(*reinterpret_cast<const u64*>(&std::get<0>(page.columns)[i])), "idx:", i, "/",
-                    page.max_num_tuples_per_page / 10, page.max_num_tuples_per_page);
+                    page.max_tuples_per_page / 10, page.max_tuples_per_page);
             //            return;
             throw std::runtime_error(send ? "send unexpected page contents!!!"
                                           : "recv unexpected page "
