37458cd1d9d93b747e2753b910348896a7968e3b
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-10-10 18:02:23 +0200

    updated exceptions

---
+++ include/exceptions/exceptions_alloc.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstring>
+#include <string>
+#include <system_error>
+
+#include "defaults.h"
+
+class AllocError : public std::runtime_error {
+  private:
+    static auto bytes_to_GiBs(u64 sz) { return std::to_string(1.0 * sz / 1e6); }
+  public:
+    explicit AllocError(const std::string& type, u64 alloc_sz)
+        : std::runtime_error("Could not allocate memory using " + type + " allocator\n" + "Tried allocating " +
+                             bytes_to_GiBs(alloc_sz) + " GiBs and got: " + std::string(strerror(errno)))
+    {
+    }
+};
+
+class MMapAllocError : public AllocError {
+  public:
+    explicit MMapAllocError(u64 alloc_sz) : AllocError("mmap", alloc_sz) {}
+};
