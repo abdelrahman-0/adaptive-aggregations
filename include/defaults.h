1feb6a2da8fa3a2acd7179a9a0e43f5ef0800566
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-10-16 18:17:55 +0200

    pass page sizes via compile time params

--- include/defaults.h
+++ include/defaults.h
@@ -16,14 +16,14 @@ using u64 = uint64_t;
 
 namespace defaults {
 
-DEFAULTS_AUTO hashtable_page_size = 1ul << 12;
+DEFAULTS_AUTO hashtable_page_size = 1ul << HT_PAGE_SIZE_POWER;
 
 DEFAULTS_AUTO local_page_size = 1ul << 12;
 DEFAULTS_AUTO local_io_depth = 256ul;
 DEFAULTS_AUTO num_pages_on_chunk = 1ul << 6;
 
 DEFAULTS_AUTO network_io_depth = 128ul;
-DEFAULTS_AUTO network_page_size = 1ul << 17;
+DEFAULTS_AUTO network_page_size = 1ul << NETWORK_PAGE_SIZE_POWER;
 DEFAULTS_AUTO kernel_recv_buffer_size = 1u << 29;
 DEFAULTS_AUTO kernel_send_buffer_size = 1u << 29;
 
