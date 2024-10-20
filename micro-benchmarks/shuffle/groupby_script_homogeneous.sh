4c60c34cdf8b983a9ef4eb407019128003fa2be1
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-10-21 00:06:03 +0200

    updated group by scripts

--- micro-benchmarks/shuffle/groupby_script_homogeneous.sh
+++ micro-benchmarks/shuffle/groupby_script_homogeneous.sh
@@ -7,7 +7,7 @@ PRINT_HEADER='--print_header'
 
 for NODES in $(seq $MAX_NODES -1 $(($LOCAL_NODE_ID + 1))); do
   for THREADS in 1 4 6 12 24 30 32; do
-    for NGROUPS in 1 10 100; do
+    for NGROUPS in 1 10 100 1000 10000 100000 1000000; do
       for TRY in $(seq 1 5); do
         if [[ "${LOCAL_NODE_ID}" == 0 ]]; then
           sleep 1s
