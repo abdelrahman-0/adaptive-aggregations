ccca6b30211aed30137c2e3ddfc377065dc0ef1d
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-11-01 00:24:12 +0100

    updated logging

--- micro-benchmarks/aggregation/local/launch.sh
+++ micro-benchmarks/aggregation/local/launch.sh
@@ -1,4 +1,4 @@
-TARGET_PATH='../../../cmake-build-release/micro-benchmarks/aggregation/aggregation_local'
+TARGET_PATH='../../../build-release/micro-benchmarks/aggregation/aggregation_local'
 
 FLAGS="--nolocal --random --npages=1000000 --morselsz=10 --pin --partitions=16 --slots=8192"
 PRINT_HEADER='--print_header'
