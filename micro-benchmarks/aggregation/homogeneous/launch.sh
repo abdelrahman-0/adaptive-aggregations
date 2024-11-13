ff029724dea414f9b89016d614c301d1585060b0
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-11-13 20:37:38 +0100

    small updates

--- micro-benchmarks/aggregation/homogeneous/launch.sh
+++ micro-benchmarks/aggregation/homogeneous/launch.sh
@@ -14,10 +14,10 @@ for NODES in $(seq $MAX_NODES -1 $(($LOCAL_NODE_ID + 1))); do
       for NGROUPS in 1 100 10000 100000 1000000 10000000; do
         for TRY in $(seq 1 5); do
           if [[ "${LOCAL_NODE_ID}" == 0 ]]; then
-            sleep 1s
+            sleep 5s
           fi
           $TARGET_PATH $PRINT_HEADER $FLAGS --nodes=$NODES --npages=$NPAGES --threads=$THREADS --groups=$NGROUPS >/dev/null
-          sleep 1s
+          sleep 5s
           PRINT_HEADER='--noprint_header'
         done
       done
