76b478aea71d94c64387d89284c877843cc82dd9
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-12-24 21:36:28 +0100

    updated local agg launch script

--- micro-benchmarks/aggregation/local/launch.sh
+++ micro-benchmarks/aggregation/local/launch.sh
@@ -1,13 +1,13 @@
 TARGET_PATH='../../../build-release/micro-benchmarks/aggregation/aggregation_local'
 
-FLAGS="--random --morselsz=1000 --pin --partitions=32 --slots=8192"
+FLAGS="--random --morselsz=1000 --pin --slots=8192"
 PRINT_HEADER='--print_header'
 
 for NPAGES in 2400000; do
   for NPARTS in 16 32 64 128 256; do
     for NGROUPS in 1 100 10000 100000 1000000 10000000; do
       for THREADS in 1 2 4 8 16 32; do
-        for TRY in $(seq 1 5); do
+        for TRY in $(seq 1 3); do
           $TARGET_PATH $PRINT_HEADER $FLAGS --npages=$NPAGES --partitions=$NPARTS --groups=$NGROUPS --threads=$THREADS >/dev/null
           PRINT_HEADER='--noprint_header'
         done
