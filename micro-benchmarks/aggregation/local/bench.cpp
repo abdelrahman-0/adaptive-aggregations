45668ba65501f00065c14a8b18e850d8d2ae2b8d
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2024-12-24 16:23:32 +0100

    updated local bench

--- micro-benchmarks/aggregation/local/bench.cpp
+++ micro-benchmarks/aggregation/local/bench.cpp
@@ -205,13 +205,13 @@ int main(int argc, char* argv[])
 #if defined(GLOBAL_OPEN_HT)
     for (u64 i : range(ht_glob.size_mask + 1)) {
         if (auto slot = ht_glob.slots[i].load()) {
-            auto slot_count   = std::get<0>(reinterpret_cast<HashtableGlobal::slot_idx_raw_t>(reinterpret_cast<uintptr_t>(slot) >> (is_ht_glob_salted ? 16 : 0))->get_aggregates());
-            auto attr_min     = std::get<1>(reinterpret_cast<HashtableGlobal::slot_idx_raw_t>(reinterpret_cast<uintptr_t>(slot) >> (is_ht_glob_salted ? 16 : 0))->get_aggregates());
-            auto attr_max     = std::get<2>(reinterpret_cast<HashtableGlobal::slot_idx_raw_t>(reinterpret_cast<uintptr_t>(slot) >> (is_ht_glob_salted ? 16 : 0))->get_aggregates());
-            auto attr_anyval  = std::get<3>(reinterpret_cast<HashtableGlobal::slot_idx_raw_t>(reinterpret_cast<uintptr_t>(slot) >> (is_ht_glob_salted ? 16 : 0))->get_aggregates());
-            // print(slot_count, attr_min, attr_max, attr_anyval);
-            count            += slot_count;
+            auto slot_count  = std::get<0>(reinterpret_cast<HashtableGlobal::slot_idx_raw_t>(reinterpret_cast<uintptr_t>(slot) >> (is_ht_glob_salted ? 16 : 0))->get_aggregates());
+            count           += slot_count;
             inserts++;
+            // auto attr_anyval  = std::get<1>(reinterpret_cast<HashtableGlobal::slot_idx_raw_t>(reinterpret_cast<uintptr_t>(slot) >> (is_ht_glob_salted ? 16 : 0))->get_aggregates());
+            // auto attr_min     = std::get<2>(reinterpret_cast<HashtableGlobal::slot_idx_raw_t>(reinterpret_cast<uintptr_t>(slot) >> (is_ht_glob_salted ? 16 : 0))->get_aggregates());
+            // auto attr_max     = std::get<3>(reinterpret_cast<HashtableGlobal::slot_idx_raw_t>(reinterpret_cast<uintptr_t>(slot) >> (is_ht_glob_salted ? 16 : 0))->get_aggregates());
+            // print(slot_count, attr_min, attr_max, attr_anyval);
         }
     }
 
