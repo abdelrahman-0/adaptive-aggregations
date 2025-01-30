99e775a71fdf55adf867ccec232f86e597b4fb18
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2025-01-30 01:13:01 +0100

    updated local bench

--- micro-benchmarks/aggregation/local/launch.sh
+++ micro-benchmarks/aggregation/local/launch.sh
@@ -1,12 +1,12 @@
 TARGET_PATH='../../../build-release/micro-benchmarks/aggregation/aggregation_local'
 
-FLAGS="--random --morselsz=1000 --nopin --slots=8192"
+FLAGS="--random --morselsz=1000 --pin"
 PRINT_HEADER='--print_header'
 
 for NPAGES in 2400000; do
   for NPARTS in 64; do
-    for NGROUPS in 100000; do
-      for THREADS in 8 16 32; do
+    for NGROUPS in 10 100000 10000000; do
+      for THREADS in 8 16; do
         for TRY in $(seq 1 3); do
           $TARGET_PATH $PRINT_HEADER $FLAGS --npages=$NPAGES --partitions=$NPARTS --groups=$NGROUPS --threads=$THREADS >/dev/null
           PRINT_HEADER='--noprint_header'
