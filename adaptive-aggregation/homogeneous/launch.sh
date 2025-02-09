f91613b18427ed57ea70e6042c68f1c7d8f8a3bf
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2025-02-09 20:04:57 +0100

    added adaptive aggregation launch scripts

---
+++ adaptive-aggregation/homogeneous/launch.sh
@@ -0,0 +1,26 @@
+TARGET_PATH='../../../build-release/adaptive-aggregation/worker_homogeneous'
+LOCAL_NODE_ID="${NODE_ID:-0}"
+
+MAX_NODES=4
+FLAGS="--nolocal --random --morselsz=100 --pin --config=../../../configs/config_aws.json"
+# --seed=${LOCAL_NODE_ID}
+PRINT_HEADER='--print_header --policy=regression --timeout=200 --static_workers=4'
+
+for NPAGES in 2400000; do
+  for THREADS in 6 12 32; do
+    for NGROUPS in 10 1000 100000 10000000; do
+      for NPARTS in 64; do
+        for PRTGRPSZ in 4; do
+          for TRY in $(seq 1 3); do
+            if [[ "${LOCAL_NODE_ID}" == 0 ]]; then
+              sleep 2s
+            fi
+            $TARGET_PATH $PRINT_HEADER $FLAGS --nodes=$NODES --npages=$NPAGES --threads=$THREADS --groups=$NGROUPS --partitions=$NPARTS --partgrpsz=$PRTGRPSZ >/dev/null
+            sleep 2s
+            PRINT_HEADER='--noprint_header'
+          done
+        done
+      done
+    done
+  done
+done
---
+++ adaptive-aggregation/launch.sh
@@ -0,0 +1,25 @@
+TARGET_PATH='../../../build-release/adaptive-aggregation/coordinator'
+LOCAL_NODE_ID="${NODE_ID:-0}"
+
+MAX_NODES=4
+FLAGS="--config=../../configs/config_aws_4_workers.json"
+PRINT_HEADER='--print_header'
+
+for NPAGES in 2400000; do
+  for THREADS in 6 12 32; do
+    for NGROUPS in 10 1000 100000 10000000; do
+      for NPARTS in 64; do
+        for PRTGRPSZ in 4; do
+          for TRY in $(seq 1 3); do
+            if [[ "${LOCAL_NODE_ID}" == 0 ]]; then
+              sleep 2s
+            fi
+            $TARGET_PATH $PRINT_HEADER $FLAGS --nodes=$MAX_NODES --npages=$NPAGES >/dev/null
+            sleep 2s
+            PRINT_HEADER='--noprint_header'
+          done
+        done
+      done
+    done
+  done
+done
