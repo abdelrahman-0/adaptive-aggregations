861b589900ad112161d735e5c714e278755fe1c8
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2025-02-10 08:33:14 +0100

    updated single-node launch

--- micro-benchmarks/aggregation/local/launch.sh
+++ micro-benchmarks/aggregation/local/launch.sh
@@ -7,7 +7,7 @@ for HTF in 2; do
   for NPAGES in 2400000; do
     for NPARTS in 64; do
       for NGROUPS in 10 1000 100000 10000000; do
-        for THREADS in 16 12 32; do
+        for THREADS in 6 12 32; do
           for TRY in $(seq 1 3); do
             $TARGET_PATH $PRINT_HEADER $FLAGS --npages=$NPAGES --partitions=$NPARTS --groups=$NGROUPS --threads=$THREADS --htfactor=$HTF >/dev/null
             PRINT_HEADER='--noprint_header'
