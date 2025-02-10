2f165f5cafab9258c32a4f82bb2cef31b16f06ce
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2025-02-10 07:10:50 +0100

    updated coord launch

--- adaptive-aggregation/launch.sh
+++ adaptive-aggregation/launch.sh
@@ -5,15 +5,17 @@ MAX_NODES=4
 FLAGS="--config=../configs/config_aws_4_workers.json"
 PRINT_HEADER='--print_header'
 
-for NPAGES in 2400000; do
-  for THREADS in 6 12 32; do
-    for NGROUPS in 10 1000 100000 10000000; do
-      for NPARTS in 64; do
-        for PRTGRPSZ in 4; do
-          for TRY in $(seq 1 3); do
-            $TARGET_PATH $PRINT_HEADER $FLAGS --nodes=$MAX_NODES --npages=$NPAGES
-            sleep 1s
-            PRINT_HEADER='--noprint_header'
+for TIMEOUT in 150 250; do
+  for NPAGES in 2400000; do
+    for THREADS in 6 12 32; do
+      for NGROUPS in 10 1000 100000 10000000; do
+        for NPARTS in 64; do
+          for PRTGRPSZ in 4; do
+            for TRY in $(seq 1 3); do
+              $TARGET_PATH $PRINT_HEADER $FLAGS --nodes=$MAX_NODES --npages=$NPAGES
+              sleep 1s
+              PRINT_HEADER='--noprint_header'
+            done
           done
         done
       done
