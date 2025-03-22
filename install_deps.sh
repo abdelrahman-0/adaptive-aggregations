fa16d1017cce871dd43d585a82b81dc7a3153400
abdelrahman-0 <abdelrahmanadel1999@yahoo.com>
2025-03-22 03:39:29 +0100

    updated deps

--- install_deps.sh
+++ install_deps.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd ~
-
 sudo apt-get -y update
-sudo apt-get -y install g++ make cmake mbw libgflags-dev libboost-all-dev iperf likwid linux-tools-6.5.0-1022-aws ncdu gdb bpfcc-tools linux-headers-$(uname -r) libjemalloc-dev
+sudo apt-get -y install g++ make cmake mbw libgflags-dev libboost-all-dev iperf linux-tools-6.5.0-1022-aws ncdu gdb bpfcc-tools linux-headers-$(uname -r) libjemalloc-dev
 
+cd ~
 # oneTBB
 cd /tmp &&
 git clone https://github.com/oneapi-src/oneTBB.git &&
@@ -14,29 +13,18 @@ cmake -DTBB_TEST=OFF .. &&
 cmake --build . &&
 sudo cmake --install .
 
-# install liburing from source (for multishot receive)
-cd ~
+# install liburing from source (latest version)
+cd /tmp &&
 git clone https://github.com/axboe/liburing.git
 cd liburing
 ./configure --cc=gcc --cxx=g++
 make -j$(nproc)
 sudo make install
 
-# install Apache's CPC sketch from source
-cd ~
-git clone https://github.com/apache/datasketches-cpp.git
-cd datasketches-cpp
-cmake -S . -B build/Release -DCMAKE_BUILD_TYPE=Release
-sudo cmake --build build/Release -t install
-cd ~
-
 # increase read and write socket buffers
 sudo sysctl -w net.core.rmem_max=500000000
 sudo sysctl -w net.core.wmem_max=500000000
 sudo sysctl -p
-
-# LIKWID needs to read MSR counters
-sudo modprobe msr
 
 # install hwdata tool
 # sudo dpkg --remove linux-intel-iotg-tools-common
